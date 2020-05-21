db232c68bb5d667a7bb67084b3cc3a08dae3a7d1
msyphus <mark.syphus@gmail.com>
2020-05-20 21:47:10 -0600

    added projects.js file, adjusted display of logos, added api route for projects

---
+++ app/data/projects.js
@@ -0,0 +1,22 @@
+var projects = [
+    {
+        name: "clean supply storage room",
+        scores: []
+    },
+    {
+        name: "design qc testing plan",
+        scores: []
+    },
+    {
+        name: "test new app",
+        scores: []
+    },
+    {
+        name: "plan department party",
+        scores: []
+    }
+];
+
+module.exports = {
+    projectKey: projects
+};
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,7 +2,11 @@ module.exports = function(app) {
     var employees = require("../data/employees");
 
     app.get("/api/employees", function(req, res) {
-        return res.json(employees);
+        res.json(employees);
+    });
+
+    app.get("api/projects", function(req, res) {
+        res.json(projects);
     });
     
     app.post("/api/employees", function(req, res) {

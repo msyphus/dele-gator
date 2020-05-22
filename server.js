2986ea9e684b0da02b651e32db6e52fd2d1a0385
msyphus <mark.syphus@gmail.com>
2020-05-21 21:59:15 -0600

    recreated css stylesheets, added dummy datay to employee array

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(express.static("app/public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 

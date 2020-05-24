0e8b22a794853dbc094cc86f76d67fadb9338d62
msyphus <mark.syphus@gmail.com>
2020-05-23 23:38:47 -0600

    stylized form, improved smoothness of employee match modal update, added extra home page content

--- app/data/employees.js
+++ app/data/employees.js
@@ -17,7 +17,7 @@ var employeeArray = [
     {
         name: "Joanna",
         photo: "https://cdn.pixabay.com/photo/2017/08/06/04/09/people-2588594_1280.jpg",
-        scores: [5, 2, 3, 3, 2, 2, 4, 5, 2, 4]
+        scores: [5, 2, 4, 3, 2, 2, 4, 5, 2, 4]
 
     }
 ];
